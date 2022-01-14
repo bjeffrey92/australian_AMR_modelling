9784de3d76a42925a2f3169002460e3c530ea7c8
Ben <bjeffrey92@hotmail.com>
2022-01-14 14:18:47 +0000

    script to run the time series models

---
+++ run_time_series_models.R
@@ -0,0 +1,56 @@
+library(MyForecaster)
+
+source("data_loader.R")
+source("EDA.R")
+
+forecasting_functions <- head(england_paper_forecasting_functions, 3)
+do_forecasts_ <- Vectorize(do_forecasts, c("forecasting_horizon"))
+
+run_forecasts <- function(data, organism, output_file, PI = 95) {
+  ab <- data$Antibiotic[[1]]
+  location <- data$Postcode[[1]]
+
+  time_series <- ts(data$Perc.R,
+        start = min(data$Year_Quarter),
+        frequency = 4
+    )
+  horizons <- 1:4 / 4
+  output <- do_forecasts_(
+        forecasting_functions,
+        time_series,
+        forecasting_horizon = horizons,
+        max_forecasting_horizon = 4 / 4,
+        location = location,
+        ab = ab,
+        PI = PI,
+        organism = organism,
+        frequency = 4
+    )
+  write_output(t(output), output_file)
+}
+
+
+main <- function(species, ab, prediction_steps = 4) {
+  df <- cache_load_data(species)
+  df <- df[df$Antibiotic == ab,]
+
+  df <- validate_data(df)
+  if (is.null(df)) {
+    return(FALSE)
+  }
+  output_file <- sprintf("time_series_result_%s_%s.tsv", species, ab)
+  for (i in group_split(df, Postcode)) {
+    run_forecasts(i, species, output_file = output_file)
+  }
+  return(TRUE)
+}
+
+
+for (i in species_ab_combos) {
+  success <- main(i[[1]], i[[2]])
+  if (success) {
+    sprintf("%s completed", i)
+  } else {
+    sprintf("insufficient data for %s", i)
+  }
+}
